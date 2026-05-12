c21b27b1e187762f175cacc0f92eb2474a3cab93
juben332 <your@email.com>
2026-05-12 20:33:45 +0800

    Resize About background to contain full image

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <ShatterLoader>
       <div style={{
         width: '100%', height: '100%',
-        background: '#000 url("/BG-about.png") center/cover no-repeat',
+        background: '#000 url("/BG-about.png") center/contain no-repeat',
       }} />
     </ShatterLoader>
   );
