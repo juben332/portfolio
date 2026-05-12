7eb74bef4f36df19ff1516c7864cd5ecc9f6e403
juben332 <your@email.com>
2026-05-12 20:31:32 +0800

    Add portrait photo as About section background

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,10 @@ import ShatterLoader from './ShatterLoader';
 export default function About() {
   return (
     <ShatterLoader>
-      <div style={{ width: '100%', height: '100%', background: '#000' }} />
+      <div style={{
+        width: '100%', height: '100%',
+        background: '#000 url("/BG-about.png") center/cover no-repeat',
+      }} />
     </ShatterLoader>
   );
 }
